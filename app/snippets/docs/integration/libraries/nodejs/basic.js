d0acdf8978b01cc4cd6a6550060b583afe93b2c5
Raphael Darley <raphael@raphaeldarley.com>
2023-09-18 16:43:07 +0100

    update node.js example app

--- app/snippets/docs/integration/libraries/nodejs/basic.js
+++ app/snippets/docs/integration/libraries/nodejs/basic.js
@@ -1,45 +1,52 @@
-const { default: Surreal } = require('surrealdb.js');
+// import as ES module or common JS
+import { Surreal } from 'surrealdb.node';
 
 const db = new Surreal();
 
 async function main() {
-
 	try {
-		// Connect to the database
-		await db.connect('http://127.0.0.1:8000/rpc');
-
-		// Signin as a namespace, database, or root user
-		await db.signin({
-			user: 'root',
-			pass: 'root',
-		});
+		// use in-memory database
+		await db.connect('memory');
+		// connect to database server
+		await db.connect('ws://127.0.0.1:8000');
+		// use rocksdb file
+		await db.connect(`rocksdb://${process.cwd()}/test.db`);
 
 		// Select a specific namespace / database
-		await db.use({ns: 'test', db: 'test'});
+		await db.use({ ns: "test", db: "test" });
 
 		// Create a new person with a random id
 		let created = await db.create("person", {
-			title: 'Founder & CEO',
+			title: "Founder & CEO",
 			name: {
-				first: 'Tobie',
-				last: 'Morgan Hitchcock',
+				first: "Tobie",
+				last: "Morgan Hitchcock",
 			},
 			marketing: true,
-			identifier: Math.random().toString(36).slice(2, 12),
+			identifier: Math.random().toString(36).substr(2, 10),
 		});
 
 		// Update a person record with a specific id
 		let updated = await db.merge("person:jaime", {
-			marketing: true,
+			marketing: true
 		});
 
 		// Select all people records
 		let people = await db.select("person");
 
 		// Perform a custom advanced query
-		let groups = await db.query('SELECT marketing, count() FROM type::table($tb) GROUP BY marketing', {
-			tb: 'person',
-		});
+		let groups = await db.query(
+			"SELECT marketing, count() FROM type::table($table) GROUP BY marketing",
+			{
+				table: "person",
+			},
+		);
+
+		// Delete all people upto but not including Jaime
+		let deleted = await db.delete("person:..jaime");
+
+		// Delete all people
+		await db.delete("person");
 
 	} catch (e) {
 
